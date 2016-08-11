090fd5add62ad154263b58f46be515b2e126b4f3
Allen Korenevsky <theplatapi@gmail.com>
2016-08-10 23:26:35 -0700

    Added a default config

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ var Papa = require('papaparse');
 
 module.exports = function(text) {
   this.cacheable();
+  this.default = {
+    header: true,
+    dynamicTyping: true,
+    comments: false
+  };
 
-  var config = loaderUtils.getLoaderConfig(this.query);
+  var config = loaderUtils.getLoaderConfig(this, "default");
   var parsed = Papa.parse(text, {
     header: config.header,
     dynamicTyping: config.dynamicTyping,
